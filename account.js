62f179bbb4cb630ae5cac9b7c805144411ac9aa8
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-12 13:56:28 +0100

    added in in each function promises

--- account.js
+++ account.js
@@ -1,102 +1,114 @@
+function makeRequest(method, url, body){
+    return new Promise(
+        function (resolve, reject){
+            let req = new XMLHttpRequest();
+            
+            req.onload = function(){
+                const data = JSON.parse(req.responseText);
+                if (req.status>=200 && req.status<300) {
+                    resolve(data);    
+                } else {
+                    const reason = new Error('Rejected');
+                    reject(reason);
+                }        
+            };
+            
+            req.open(method, url);
+            if(body==null){
+                req.send();
+            }
+            else {
+                req.send(JSON.stringify(body));
+            }
+        }
+    );
+}
+
 function getAccount(){
     let id = Number(document.getElementById("anAcc").value);
-    let req = new XMLHttpRequest();
-    req.onload= function(){
-
-        const data = JSON.parse(req.responseText);
-        const firstName = data.firstName;
-        const lastName = data.lastName;
-        document.getElementById("accountFirstName").innerHTML=firstName;
-        document.getElementById("accountLastName").innerHTML=lastName;
-    };
-req.open("GET",`http://localhost:8080/AccountSETemplate/api/account/searchAccounts/${id}`);
-req.send();
-    
+    makeRequest("GET",`http://localhost:8080/AccountSETemplate/api/account/searchAccounts/${id}`)
+        .then((data) => {
+            const firstName = data.firstName;
+            const lastName = data.lastName;
+            document.getElementById("accountFirstName").innerHTML=firstName;
+            document.getElementById("accountLastName").innerHTML=lastName;
+            console.log(data);
+    })
+        .catch((error) => {
+            console.log(error.message);
+    });
     return false;
 }
 
-
 function getAllAccounts(){
-    let req = new XMLHttpRequest();
-    req.onload= function(){
- 
-        console.log(req.responseText);
-        const data = JSON.parse(req.responseText);
-        
-        let allnames = " ";
-        
-        const container = document.getElementById('accountsDiv');
-        
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);
-        }
-        
-        for(let i=0; i<data.length;i++){
- 
-            
-            let myP = document.createElement('p');
-            myP.innerHTML = data[i].firstName;
-            myP.setAttribute('name', 'acc-text')
-            container.appendChild(myP);
-        }
-    }
- req.open("GET","http://localhost:8080/AccountSETemplate/api/account/getAllAccounts");
-    req.send();
-
+    makeRequest("GET","http://localhost:8080/AccountSETemplate/api/account/getAllAccounts")
+        .then((data) => {
+            let allnames = " ";
+            const container = document.getElementById('accountsDiv');
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+            for(let i=0; i<data.length;i++){
+                let myP = document.createElement('p');
+                myP.innerHTML = data[i].firstName;
+                myP.setAttribute('name', 'acc-text')
+                container.appendChild(myP);
+            }
+    })
+        .catch((error) => {
+            console.log(error.message);
+        });
      return false;
 }
 
 function createAccount(){
-    let req = new XMLHttpRequest();
     let newAcc = {
-    firstName: document.getElementById("accFirstName").value,
-    lastName: document.getElementById("accLastName").value,
-    accountNumber: document.getElementById("accNumber").value
-};
-    
-    req.onload= function(){
-        console.log(req.responseText);
-        const data = JSON.parse(req.responseText);
-        
-    }
-     req.open("POST","http://localhost:8080/AccountSETemplate/api/account/createAccounts");
-    req.send(JSON.stringify(newAcc));
+        firstName: document.getElementById("accFirstName").value,
+        lastName: document.getElementById("accLastName").value,
+        accountNumber: document.getElementById("accNumber").value
+     };
+    makeRequest("POST","http://localhost:8080/AccountSETemplate/api/account/createAccounts",newAcc)
+        .then((data) => {
+            console.log(data);
+    })
+        .catch((error) => {
+            console.log(error.message);
+    });
     
     return false;
 }
 
 
-
 function deleteAccount(){
     let accountNumber = Number(document.getElementById("deleteAcc").value);
-    let req = new XMLHttpRequest();
-    req.onload= function(){
-        console.log(req.responseText);
-        const data = JSON.parse(req.responseText);
-
-    };
-req.open("DELETE",`http://localhost:8080/AccountSETemplate/api/account/deleteAccounts/${accountNumber}`);
-req.send();
+    makeRequest("DELETE",`http://localhost:8080/AccountSETemplate/api/account/deleteAccounts/${accountNumber}`)
+        .then((data) => {
+            console.log(data);
+    })
+        .catch((error) => {
+            console.log(error.message);
+    });
     return false;
 }
 
 
 function updateAccount(){
     let id = Number(document.getElementById("updateId").value);
-    let req = new XMLHttpRequest();
-    
     let updateAcc = {
     firstName: document.getElementById("updateFirstName").value,
     lastName: document.getElementById("updateLastName").value,
     accountNumber: document.getElementById("updateNumber").value
-};
-    
-    req.onload= function(){
-
-        const data = JSON.parse(req.responseText);
-
     };
-req.open("PUT",`http://localhost:8080/AccountSETemplate/api/account/updateAccounts/${id}`);
-req.send(JSON.stringify(updateAcc));
+    makeRequest("PUT",`http://localhost:8080/AccountSETemplate/api/account/updateAccounts/${id}`, updateAcc)
+        .then((data) => {
+            console.log(data);
+    })
+        .catch((error) => {
+            console.log(error.message);
+    });
+
     return false;
 }
+
+
+
